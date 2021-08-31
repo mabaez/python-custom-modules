59a69899a25e93b41e74be1e63049dd03676e184
Miguel Baez <mabaez@users.noreply.github.com>
2021-08-31 15:05:57 -0400

    Create new file
    Fichero para la creación de la clase Selección

---
+++ Seleccion.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 29 17:20:06 2021
+
+@author: Miguel Ángel Báez
+license: GPL
+version: 0.11
+mantainer: Miguel Ángel Báez
+email : braintree75.colab@gmail.com
+twitter: @mabaez
+status : WIP
+
+"""
+# Creamos una clase
+class Selección:
+    # Contenido de la clase
+    # La definimos e inicializamos - sus atributos entre paréntesis
+    def __init__(self, lance, respuesta):
+        self.lance = lance
+        self.respuesta = respuesta
